e7b478a2a3ea51317df95cb3d0fee23d2f32717c
MZ <mario.zambrano@udea.edu.co>
2024-05-26 17:21:20 -0500

    Modificacion de la vista, arreglo en la disposicion de opciones en el menu inicial y en el menu de login

--- modelo.py
+++ modelo.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import cv2
 import pydicom
 
+
 def crear_BDSQL():
     cnx = mysql.connector.connect(
         user='root',
@@ -22,7 +23,6 @@ def crear_BDSQL():
     cursorSQL.execute("GRANT ALL PRIVILEGES ON alzcare.* TO 'admin'@'localhost';")
     cursorSQL.close()
     cnx.close()
-
 def conectar_SQL():
     SERVIDOR = 'localhost'
     USUARIO = 'admin'
@@ -31,12 +31,10 @@ def conectar_SQL():
     cnx = mysql.connector.connect(user=USUARIO,password=CONTRASEÑA,host=SERVIDOR,database=DB)
     cursorSQL = cnx.cursor()
     return cursorSQL,cnx
-
 def desconectar_SQL(cnx,cursor):
     cnx.commit()
     cursor.close()
     cnx.close()
-
 def crear_tablasSQL():
     cursorSQL, cnx = conectar_SQL()
     cursorSQL.execute("USE alzcare;")
@@ -44,3 +42,15 @@ def crear_tablasSQL():
     cursorSQL.execute("CREATE TABLE IF NOT EXISTS paciente (ID INT UNSIGNED PRIMARY KEY,ID_Cuidador INT UNSIGNED NOT NULL,Nombre VARCHAR(225) NOT NULL,Apellido VARCHAR(225) NOT NULL,Nacimiento VARCHAR(225) NOT NULL,Procedencia VARCHAR(225) NOT NULL,Fase VARCHAR(225) NOT NULL,Escolaridad VARCHAR(225) NOT NULL,Mano_dominante VARCHAR(225) NOT NULL,FOREIGN KEY (ID_Cuidador) REFERENCES cuidador(ID) ON UPDATE CASCADE ON DELETE CASCADE)")
     cursorSQL.execute("CREATE TABLE IF NOT EXISTS registro (ID INT UNSIGNED PRIMARY KEY,ID_Paciente INT UNSIGNED NOT NULL,Fecha_Registro VARCHAR(225) NOT NULL,Pregunta_1 INT NOT NULL,Pregunta_2 INT NOT NULL,Pregunta_3 INT NOT NULL,Pregunta_4 INT NOT NULL,Pregunta_5 INT NOT NULL,Pregunta_6 INT NOT NULL,Pregunta_7 INT NOT NULL,Pregunta_8 INT NOT NULL,Pregunta_9 INT NOT NULL,Pregunta_10 INT NOT NULL,Pregunta_11 INT NOT NULL,Pregunta_12 INT NOT NULL,Pregunta_13 INT NOT NULL,Pregunta_14 INT NOT NULL,Pregunta_15 INT NOT NULL,Pregunta_16 INT NOT NULL,Pregunta_17 INT NOT NULL,FOREIGN KEY (ID_Paciente) REFERENCES paciente(ID) ON UPDATE CASCADE ON DELETE CASCADE)")
     desconectar_SQL(cnx,cursorSQL)
+class Cuidador:
+
+    def __init__(self, ID, Nombre, Apellido, Telefono, Cedula, Formacion, Usuario, Contraseña):
+        self.ID = ID
+        self.Nombre = Nombre
+        self.Apellido = Apellido
+        self.Telefono = Telefono
+        self.Cedula = Cedula
+        self.Formacion = Formacion
+        self.Usuario = Usuario
+        self.Contraseña = Contraseña
+
--- vista.py
+++ vista.py
@@ -1,151 +1,213 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QStackedWidget, QLineEdit, QFormLayout
-from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QStackedWidget, QLineEdit, QFormLayout, QComboBox
+from PyQt5.QtCore import QTimer, Qt
 
-class InitialWindow(QWidget):
+class Advertencia(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Aviso de Seguridad')
         self.setGeometry(100, 100, 640, 480)
         self.setStyleSheet("background-color: green;")
         
-        layout = QVBoxLayout()
-        label = QLabel('''!ADIVOS DE SALUD Y SEGURIDAD\nPara el ingreso de info se debe de tener al cuidador en la sala''')
-        label.setStyleSheet("color: white; font-size: 18px;")
-        layout.addWidget(label)
-        self.setLayout(layout)
+        setup = QVBoxLayout()
+        texto_1 = QLineEdit('!ADIVOS DE SALUD Y SEGURIDAD¡')
+        texto_1.setStyleSheet("color: white; font-size: 35px;")
+        texto_1.setReadOnly(True)
+        texto_1.setAlignment(Qt.AlignCenter)
+        setup.addWidget(texto_1)
+        texto_2 = QLabel('Para el ingreso de info se debe de tener al cuidador en la sala')
+        texto_2.setStyleSheet("color: white; font-size: 30px;")
+        texto_2.setAlignment(Qt.AlignCenter)
+        setup.addWidget(texto_2)
+        self.setLayout(setup)
 
-class MainWindow(QWidget):
+class Menu_inicial(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Ventana de Inicio')
-        self.setGeometry(100, 100, 1280, 720)
-        self.setStyleSheet("background-color: gray;")
-        
-        layout = QVBoxLayout()
-        self.login_button = QPushButton('Log IN')
-        self.register_button = QPushButton('Registro')
-        self.moka_button = QPushButton('MOKA')
-        self.exit_button = QPushButton('Salir')
-        
-        layout.addWidget(self.login_button)
-        layout.addWidget(self.register_button)
-        layout.addWidget(self.moka_button)
-        layout.addWidget(self.exit_button)
-        
-        self.setLayout(layout)
+        self.setGeometry(100, 100, 640, 480)
 
-class LoginWindow(QWidget):
+        setup = QVBoxLayout()
+        organizador = QFormLayout()
+
+        titulo = QLineEdit('Bienvenido a AlzCare')
+        titulo.setStyleSheet("font-size: 30px; border: 1px solid black;")
+        titulo.setAlignment(Qt.AlignCenter)
+        titulo.adjustSize()
+        titulo.setReadOnly(True)
+        
+        self.boton_login = QPushButton('Login')
+        self.boton_login.setFixedSize(200, 50)
+        self.boton_pre_registro = QPushButton('Registro')
+        self.boton_pre_registro.setFixedSize(200, 50)
+        self.boton_moca = QPushButton('MOCA')
+        self.boton_moca.setFixedSize(200, 50)
+        self.boton_salida = QPushButton('Salir')
+        self.boton_salida.setFixedSize(200, 50)
+
+        organizador.addRow(self.boton_login)
+        organizador.addRow(self.boton_pre_registro)
+        organizador.addRow(self.boton_moca)
+        organizador.addRow(self.boton_salida)
+        organizador.setFormAlignment(Qt.AlignCenter)
+        organizador.setLabelAlignment(Qt.AlignCenter)
+        organizador.setSpacing(15)
+        organizador.setContentsMargins(50, 50, 50, 50)
+
+        setup.addWidget(titulo)
+        setup.addLayout(organizador)
+        
+        self.setLayout(setup)
+
+class Menu_Login(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Login')
-        self.setGeometry(100, 100, 1280, 720)
-        self.setStyleSheet("background-color: blue;")
+        self.setGeometry(100, 100, 320, 240)
         
-        layout = QVBoxLayout()
-        form_layout = QFormLayout()
+        setup = QVBoxLayout()
+        organizador = QFormLayout()
         
-        self.username_input = QLineEdit()
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.Password)
+        self.usuario_respuesta = QLineEdit()
+        self.contraseña_respuesta = QLineEdit()
+        self.contraseña_respuesta.setEchoMode(QLineEdit.Password)
         
-        form_layout.addRow('Usuario:', self.username_input)
-        form_layout.addRow('Contraseña:', self.password_input)
-        
-        self.login_button = QPushButton('Entrar')
-        self.exit_button = QPushButton('Salir')
-        
-        layout.addLayout(form_layout)
-        layout.addWidget(self.login_button)
-        layout.addWidget(self.exit_button)
-        
-        self.setLayout(layout)
+        organizador.addRow('Usuario:', self.usuario_respuesta)
+        organizador.addRow('Contraseña:', self.contraseña_respuesta)
+        organizador.setFormAlignment(Qt.AlignCenter)
+        organizador.setLabelAlignment(Qt.AlignCenter)
+        organizador.setSpacing(15)
+        organizador.setContentsMargins(50, 50, 50, 50)
 
-class RegistroWindow(QWidget):
+        self.boton_login = QPushButton('Entrar')
+        self.boton_salida = QPushButton('Salir')
+        
+        setup.addLayout(organizador)
+        setup.addWidget(self.boton_login)
+        setup.addWidget(self.boton_salida)
+        
+        self.setLayout(setup)
+
+class Menu__pre_registro(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Pre-Registro')
+        self.setGeometry(100, 100, 640, 480)
+
+        setup = QVBoxLayout()
+        organizador = QFormLayout()
+
+        self.boton_registro = QPushButton('Registrar')
+        self.boton_registro.setFixedSize(200, 50)
+        self.boton_importar = QPushButton('Importar')
+        self.boton_importar.setFixedSize(200, 50)
+        self.boton_salida = QPushButton('Salir')
+        self.boton_salida.setFixedSize(200, 50)
+
+        organizador.addRow(self.boton_registro)
+        organizador.addRow(self.boton_importar)
+        organizador.addRow(self.boton_salida)
+        organizador.setFormAlignment(Qt.AlignCenter)
+        organizador.setLabelAlignment(Qt.AlignCenter)
+        organizador.setSpacing(15)
+        organizador.setContentsMargins(50, 50, 50, 50)
+
+        setup.addLayout(organizador)
+
+        self.setLayout(setup)
+class Menu_registro(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Registro')
-        self.setGeometry(100, 100, 1280, 720)
-        self.setStyleSheet("background-color: pink;")
+        self.setGeometry(100, 100, 640, 480)
         
-        layout = QVBoxLayout()
-        form_layout = QFormLayout()
+        setup = QVBoxLayout()
+        organizador = QFormLayout()
         
-        self.username_input = QLineEdit()
-        self.password_input = QLineEdit()
-        self.password_input.setEchoMode(QLineEdit.Password)
-        self.name_input = QLineEdit()
-        self.cc_input = QLineEdit()
-        self.phone_input = QLineEdit()
+        self.usuario_respuesta = QLineEdit()
+        self.contraseña_respuesta = QLineEdit()
+        self.contraseña_respuesta.setEchoMode(QLineEdit.Password)
+        self.nombre_cuidador = QLineEdit()
+        self.cedula_cuidador = QLineEdit()
+        self.cecular_cuidador = QLineEdit()
         
-        form_layout.addRow('Usuario:', self.username_input)
-        form_layout.addRow('Contraseña:', self.password_input)
-        form_layout.addRow('Nombre:', self.name_input)
-        form_layout.addRow('CC:', self.cc_input)
-        form_layout.addRow('Teléfono:', self.phone_input)
-        
-        self.register_button = QPushButton('Registrar')
-        
-        layout.addLayout(form_layout)
-        layout.addWidget(self.register_button)
-        
-        self.setLayout(layout)
+        self.opciones = QComboBox()
+        self.opciones.addItem("Opción 1")
+        self.opciones.addItem("Opción 2")
+        self.opciones.addItem("Opción 3")
 
-class MOKAWindow(QWidget):
+        organizador.addRow('Usuario:', self.usuario_respuesta)
+        organizador.addRow('Contraseña:', self.contraseña_respuesta)
+        organizador.addRow('Nombre:', self.nombre_cuidador)
+        organizador.addRow('CC:', self.cedula_cuidador)
+        organizador.addRow('Teléfono:', self.cecular_cuidador)
+        organizador.addRow('Formación:', self.opciones)
+        organizador.setFormAlignment(Qt.AlignCenter)
+        organizador.setLabelAlignment(Qt.AlignCenter)
+        organizador.setSpacing(15)
+        organizador.setContentsMargins(50, 50, 50, 50)
+        
+        self.boton_registro = QPushButton('Registrar')
+        
+        setup.addLayout(organizador)
+        setup.addWidget(self.boton_registro)
+        
+        self.setLayout(setup)
+
+class Menu_moca(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('MOKA')
-        self.setGeometry(100, 100, 1280, 720)
-        self.setStyleSheet("background-color: red;")
+        self.setWindowTitle('MOcA')
+        self.setGeometry(100, 100, 640, 480)
         
-        layout = QVBoxLayout()
-        self.label = QLabel('Opcional: NO SE GUARDA')
-        self.label.setStyleSheet("color: white; font-size: 18px;")
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+        setup = QVBoxLayout()
+        self.texto = QLabel('Opcional: NO SE GUARDA')
+        self.texto.setStyleSheet("color: white; font-size: 18px;")
+        setup.addWidget(self.texto)
+        self.setLayout(setup)
 
 class MainApp(QStackedWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('AlzCare')
-        self.setGeometry(100, 100, 1280, 720)
+        self.setGeometry(100, 100, 640, 480)
         
-        self.initial_window = InitialWindow()
-        self.main_window = MainWindow()
-        self.login_window = LoginWindow()
-        self.registro_window = RegistroWindow()
-        self.moka_window = MOKAWindow()
+        self.advertencia = Advertencia()
+        self.menu_inicial = Menu_inicial()
+        self.menu_login = Menu_Login()
+        self.menu_registro = Menu_registro()
+        self.menu_moca = Menu_moca()
         
-        self.addWidget(self.initial_window)
-        self.addWidget(self.main_window)
-        self.addWidget(self.login_window)
-        self.addWidget(self.registro_window)
-        self.addWidget(self.moka_window)
+        self.addWidget(self.advertencia)
+        self.addWidget(self.menu_inicial)
+        self.addWidget(self.menu_login)
+        self.addWidget(self.menu_registro)
+        self.addWidget(self.menu_moca)
         
-        QTimer.singleShot(3000, self.show_main_window)
+        QTimer.singleShot(5000, self.show_main_window)
         
-        self.main_window.login_button.clicked.connect(self.show_login_window)
-        self.main_window.register_button.clicked.connect(self.show_registro_window)
-        self.main_window.moka_button.clicked.connect(self.show_moka_window)
-        self.main_window.exit_button.clicked.connect(self.close)
+        self.menu_inicial.boton_login.clicked.connect(self.show_login_window)
+        self.menu_inicial.boton_pre_registro.clicked.connect(self.show_registro_window)
+        self.menu_inicial.boton_moca.clicked.connect(self.show_moca_window)
+        self.menu_inicial.boton_salida.clicked.connect(self.close)
         
-        self.login_window.login_button.clicked.connect(self.show_main_window)
-        self.login_window.exit_button.clicked.connect(self.close)
+        self.menu_login.boton_login.clicked.connect(self.show_main_window)
+        self.menu_login.boton_salida.clicked.connect(self.close)
         
     def show_main_window(self):
-        self.setCurrentWidget(self.main_window)
+        self.setCurrentWidget(self.menu_inicial)
         
     def show_login_window(self):
-        self.setCurrentWidget(self.login_window)
+        self.setCurrentWidget(self.menu_login)
         
     def show_registro_window(self):
-        self.setCurrentWidget(self.registro_window)
+        self.setCurrentWidget(self.menu_registro)
         
-    def show_moka_window(self):
-        self.setCurrentWidget(self.moka_window)
+    def show_moca_window(self):
+        self.setCurrentWidget(self.menu_moca)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_app = MainApp()
-    main_app.show()
+    window = MainApp()
+    window.show()
     sys.exit(app.exec_())
